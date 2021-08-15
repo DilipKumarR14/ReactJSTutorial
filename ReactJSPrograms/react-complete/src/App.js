f54c077ae3d3859ce9320976f7701134062dc281
Dilip <dilipkumar14inc@gmail.com>
2021-08-15 11:33:18 +0530

    Spliting the components - spliting the Date component from ExpenseItem

--- ReactJSPrograms/react-complete/src/App.js
+++ ReactJSPrograms/react-complete/src/App.js
@@ -20,6 +20,9 @@ function App() {
       <ExpenseItem title={expense[1].title}
                    amount={expense[1].amount}
                    date={expense[1].date} ></ExpenseItem>
+      <ExpenseItem title={expense[2].title}
+                   amount={expense[2].amount}
+                   date={expense[2].date} ></ExpenseItem>
     </div>
   );
 }
